7aca167467e003a75e09a242423bf372f49edbd3
高金旭 <10928033@qq.com>
2020-09-24 17:34:34 +0800

    推送

---
+++ LocalStorageHelper.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorLoginTest
+{
+	/// <summary>
+	/// 操作LocalStorage帮助类
+	/// </summary>
+	public class LocalStorageHelper
+    {
+        private readonly IJSRuntime _jsRuntime;
+        public LocalStorageHelper(IJSRuntime jsRuntime)
+        {
+            _jsRuntime = jsRuntime;
+        }
+        /// <summary>
+        /// 设置LocalStorage
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public async Task SetLocalStorage(string key, string value)
+        {
+            await _jsRuntime.InvokeVoidAsync("LocalStorageSet", key, value);
+        }
+        /// <summary>
+        /// 获取LocalStorage
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns></returns>
+        public async Task<string> GetLocalStorage(string key)
+        {
+            return await _jsRuntime.InvokeAsync<string>("LocalStorageGet", key);
+        }
+    }
+}
